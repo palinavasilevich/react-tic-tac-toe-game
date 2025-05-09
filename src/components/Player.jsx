90b3bb5027e82bbb12df6c810a31ee5e29aed33a
pvslvch <polina.vasilevichh@gmail.com>
2025-05-09 16:08:34 +0200

    feat: implement Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (e) => {
+    setPlayerName(e.target.value);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+
+        <span className="player-symbol">{symbol}</span>
+      </span>
+
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
